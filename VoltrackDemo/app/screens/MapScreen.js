b1b8744ffda0c70688de16a1e37d433171cf7e5f
silvazc <carlossz.206@gmail.com>
2021-01-15 22:42:50 -0800

    Add ability to track current user on map with an icon. Add temporary map button

--- VoltrackDemo/app/screens/MapScreen.js
+++ VoltrackDemo/app/screens/MapScreen.js
@@ -1,21 +1,205 @@
-import React, { Component } from 'react';
-import { Button } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+// https://medium.com/quick-code/react-native-location-tracking-14ab2c9e2db8
 
-function MapScreen(props) {
-    return (
-        <MapView
-         style={{ flex: 1 }}
-         provider={PROVIDER_GOOGLE}
-         showsUserLocation
-         initialRegion={{
-         latitude: 48.769768,
-         longitude: -122.485886,
-         latitudeDelta: 0.0922,
-         longitudeDelta: 0.0421}}
-      />
-      
-    );
+import React from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    Image,
+    TouchableOpacity,
+    Platform,
+    PermissionsAndroid
+} from "react-native";
+import MapView, {
+    Marker,
+    Callout,
+    AnimatedRegion,
+    Polyline,
+    PROVIDER_GOOGLE
+} from "react-native-maps";
+import haversine from "haversine";
+
+
+const LATITUDE_DELTA = 0.009;
+const LONGITUDE_DELTA = 0.009;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+
+class MapScreen extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            routeCoordinates: [],
+            distanceTravelled: 0,
+            prevLatLng: {},
+            coordinate: new AnimatedRegion({
+                latitude: LATITUDE,
+                longitude: LONGITUDE,
+                latitudeDelta: 0,
+                longitudeDelta: 0
+            })
+        };
+    }
+
+    componentDidMount() {
+        const { coordinate } = this.state;
+
+        this.watchID = navigator.geolocation.watchPosition(
+            position => {
+                const { routeCoordinates, distanceTravelled } = this.state;
+                const { latitude, longitude } = position.coords;
+
+                const newCoordinate = {
+                    latitude,
+                    longitude
+                };
+
+                if (Platform.OS === "android") {
+                    if (this.marker) {
+                        this.marker._component.animateMarkerToCoordinate(
+                            newCoordinate,
+                            500
+                        );
+                    }
+                } else {
+                    coordinate.timing(newCoordinate).start();
+                }
+
+                this.setState({
+                    latitude,
+                    longitude,
+                    routeCoordinates: routeCoordinates.concat([newCoordinate]),
+                    distanceTravelled:
+                        distanceTravelled + this.calcDistance(newCoordinate),
+                    prevLatLng: newCoordinate
+                });
+            },
+            error => console.log(error),
+            {
+                enableHighAccuracy: true,
+                timeout: 20000,
+                maximumAge: 1000,
+                distanceFilter: 10
+            }
+        );
+    }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchID);
+    }
+
+    getMapRegion = () => ({
+        latitude: this.state.latitude,
+        longitude: this.state.longitude,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+    });
+
+    calcDistance = newLatLng => {
+        const { prevLatLng } = this.state;
+        return haversine(prevLatLng, newLatLng) || 0;
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    provider={PROVIDER_GOOGLE}
+                    showUserLocation
+                    followUserLocation
+                    loadingEnabled
+                    region={this.getMapRegion()}
+                >
+                    <Polyline coordinates={this.state.routeCoordinates} strokeWidth={5} />
+                    <Marker.Animated
+                        ref={marker => {
+                            this.marker = marker;
+                        }}
+                        coordinate={this.state.coordinate}
+                    >
+                        <Image source={require('../assets/skull.png')}
+                               style={styles.tinyLogo}
+                        />
+                    </Marker.Animated>
+                </MapView>
+                <Callout>
+                    <View style = {styles.calloutView}>
+                        <TextInput style = {styles.calloutSearch}
+                                   placeholder={"Search"}
+                        />
+                    </View>
+                </Callout>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={[styles.bubble, styles.button]}>
+                        <Text style={styles.bottomBarContent}>
+                            {parseFloat(this.state.distanceTravelled).toFixed(2)} km
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject
+    },
+    bubble: {
+        flex: 1,
+        backgroundColor: "rgba(255,255,255,0.7)",
+        paddingHorizontal: 18,
+        paddingVertical: 12,
+        borderRadius: 20
+    },
+    latlng: {
+        width: 200,
+        alignItems: "stretch"
+    },
+    button: {
+        width: 80,
+        paddingHorizontal: 12,
+        alignItems: "center",
+        marginHorizontal: 10
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        marginVertical: 20,
+        backgroundColor: "transparent"
+    },
+    // TODO: Fix positioning of search button
+    calloutView: {
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        backgroundColor: "rgba(255, 255, 255, 0.9)",
+        borderRadius: 10,
+        width: "40%",
+        marginLeft: "30%",
+        marginRight: "30%",
+        marginTop: "15%"
+    },
+    calloutSearch: {
+        borderColor: "transparent",
+        marginLeft: 10,
+        width: "90%",
+        marginRight: 10,
+        height: 40,
+        borderWidth: 0.0
+    },
+    tinyLogo: {
+        width: 50,
+        height: 50,
+    }
+});
 
 export default MapScreen;
