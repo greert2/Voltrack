b1ecc9d1244e1366af5f63a8936c4a82d65c2822
tranh6 <tranh6@wwu.edu>
2021-02-18 03:58:54 -0800

    worked on map icons

--- VoltrackDemo/app/styles/MapStyles.js
+++ VoltrackDemo/app/styles/MapStyles.js
@@ -17,6 +17,37 @@ const styles = StyleSheet.create({
         paddingVertical: 12,
         borderRadius: 20
     },
+    bubbleMarker: {
+        flexDirection: 'column',
+        alignSelf: 'flex-start',
+        backgroundColor: '#fff',
+        borderRadius: 6,
+        borderColor: '#ccc',
+        borderWidth: 0.5,
+        padding: 15,
+        width: 150,
+    },
+    // Arrow below the bubbleMarker
+    arrow: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#fff',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -32,
+    },
+    arrowBorder: {
+        backgroundColor: 'transparent',
+        borderColor: 'transparent',
+        borderTopColor: '#007a87',
+        borderWidth: 16,
+        alignSelf: 'center',
+        marginTop: -0.5,
+    },
+    name: {
+        fontSize: 16,
+        marginBottom: 5,
+    },
     latlng: {
         width: 200,
         alignItems: "stretch"
